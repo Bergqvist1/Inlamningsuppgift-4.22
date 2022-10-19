5d0d54649c16394a6c9a3c6f29980cb7836535ac
Admin <Admin@TEB123021>
2022-10-19 11:47:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Uppgift_4_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 30;
+              while(i > 9)
+              {
+                Console.WriteLine(i);   
+                i--;
+              }
+
+            int l = 200;
+            while(l > 179)
+            {
+                Console.WriteLine(l);
+                l--;
+            }
+
+            int j = 1000;
+            while(j < 1401)
+            {
+                Console.WriteLine(j);
+                j = j + 50;
+
+            }
+
+            
+        }
+    }
+}
